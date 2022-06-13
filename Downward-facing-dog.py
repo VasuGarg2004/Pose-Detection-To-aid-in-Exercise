09e618eaae4e3ceecfe58fb4ede2f43d2f5ae1c5
Vasu Garg <88959104+VasuGarg2004@users.noreply.github.com>
2022-06-13 08:02:01 +0530

    Add files via upload

---
+++ Downward-facing-dog.py
@@ -0,0 +1,134 @@
+import os
+os.system('py -m pip install mediapipe opencv-python pyttsx3')
+
+import pyttsx3
+import cv2
+import mediapipe as mp
+import numpy as np
+
+mp_drawing = mp.solutions.drawing_utils
+mp_pose = mp.solutions.pose
+
+engine = pyttsx3.init("sapi5")
+voice = engine.getProperty("voices")
+engine.setProperty('voice',voice[0].id)
+
+def speak(msg):
+    engine.say(msg)
+    engine.runAndWait()
+
+cap = cv2.VideoCapture(0)
+
+# Curl counter variables
+counter = 0 
+stage = None
+
+i = 0
+
+## Setup mediapipe instance
+with mp_pose.Pose(min_detection_confidence=0.5, min_tracking_confidence=0.5) as pose:
+    while cap.isOpened():
+        ret, frame = cap.read()
+        
+        # Recolor image to RGB
+        image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        image.flags.writeable = False
+      
+        # Make detection
+        results = pose.process(image)
+    
+        # Recolor back to BGR
+        image.flags.writeable = True
+        image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+        
+        # Extract landmarks
+        try:
+            landmarks = results.pose_landmarks.landmark
+            
+             # Get coordinates
+            shoulder = [landmarks[mp_pose.PoseLandmark.LEFT_SHOULDER.value].x,landmarks[mp_pose.PoseLandmark.LEFT_SHOULDER.value].y]
+            elbow = [landmarks[mp_pose.PoseLandmark.LEFT_ELBOW.value].x,landmarks[mp_pose.PoseLandmark.LEFT_ELBOW.value].y]
+            wrist = [landmarks[mp_pose.PoseLandmark.LEFT_WRIST.value].x,landmarks[mp_pose.PoseLandmark.LEFT_WRIST.value].y]
+            hip = [landmarks[mp_pose.PoseLandmark.LEFT_HIP.value].x,landmarks[mp_pose.PoseLandmark.LEFT_HIP.value].y]
+            knee = landmarks[mp_pose.PoseLandmark.LEFT_KNEE.value].x,landmarks[mp_pose.PoseLandmark.LEFT_KNEE.value].y
+            ankle = landmarks[mp_pose.PoseLandmark.LEFT_ANKLE.value].x,landmarks[mp_pose.PoseLandmark.LEFT_ANKLE.value].y
+            
+            def calculate_angle(a,b,c):
+                a = np.array(a) # First
+                b = np.array(b) # Mid
+                c = np.array(c) # End
+                
+                radians = np.arctan2(c[1]-b[1], c[0]-b[0]) - np.arctan2(a[1]-b[1], a[0]-b[0])
+                angle = np.abs(radians*180.0/np.pi)
+    
+                if angle >180.0:
+                    angle = 360-angle
+        
+                return angle 
+            
+            # Calculate angle
+            angle1 = calculate_angle(shoulder, elbow, wrist)
+            angle2 = calculate_angle(knee, hip, shoulder)
+            angle3 = calculate_angle(ankle, knee, hip)
+            # Visualize angle
+            cv2.putText(image, str(angle1), 
+                           tuple(np.multiply(elbow, [640, 480]).astype(int)), 
+                           cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2, cv2.LINE_AA
+                                )
+            cv2.putText(image, str(angle2), 
+                           tuple(np.multiply(hip, [640, 480]).astype(int)), 
+                           cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2, cv2.LINE_AA
+                                )
+            cv2.putText(image, str(angle3), 
+                           tuple(np.multiply(knee, [640, 480]).astype(int)), 
+                           cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2, cv2.LINE_AA
+                                )
+            
+            # pose judging logic
+            if angle1 > 170 and angle2 > 52 and angle2 < 67 and angle3 > 170:
+                stage = "Correct"
+            else:
+                stage="Incorrect"
+
+            if i == 0: flag = 0
+            if stage == "Correct" and flag == 0:
+                speak(stage)
+                flag = 1
+            else:
+                if stage == "Incorrect" and flag == 1:                      
+                    speak(stage)
+                    flag = 0
+            i += 1
+
+        except:
+            pass
+        
+        # Render pose status
+        # Setup status box
+        cv2.rectangle(image, (0,0), (225,73), (245,117,16), -1)
+        
+        # Rep data
+        cv2.putText(image, 'Pose', (15,12), 
+                    cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0,0,0), 1, cv2.LINE_AA)
+        
+        # Stage data
+        cv2.putText(image, 'Stage', (65,12), 
+                    cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0,0,0), 1, cv2.LINE_AA)
+        cv2.putText(image, stage, 
+                    (60,60), 
+                    cv2.FONT_HERSHEY_SIMPLEX, 1, (255,255,255), 2, cv2.LINE_AA)
+        
+        
+        # Render detections
+        mp_drawing.draw_landmarks(image, results.pose_landmarks, mp_pose.POSE_CONNECTIONS,
+                                mp_drawing.DrawingSpec(color=(245,117,66), thickness=2, circle_radius=2), 
+                                mp_drawing.DrawingSpec(color=(245,66,230), thickness=2, circle_radius=2) 
+                                 )               
+        
+        cv2.imshow('Mediapipe Feed', image)
+
+        if cv2.waitKey(10) & 0xFF == ord('q'):
+            break
+
+    cap.release()
+    cv2.destroyAllWindows()
